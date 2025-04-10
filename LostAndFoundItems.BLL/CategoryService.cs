a6ebed8526d2417b4183d58cf01a31b2dd4d07d1
Lateef Adeyekun <lateef.adeyekun@gmail.com>
2025-04-10 10:44:52 -0500

    Category service added

---
+++ LostAndFoundItems.BLL/CategoryService.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using LostAndFoundItems.Common.DTOs;
+using LostAndFoundItems.Common;
+using LostAndFoundItems.DAL;
+using LostAndFoundItems.Models;
+
+namespace LostAndFoundItems.BLL {
+    public class CategoryService {
+        public readonly CategoryRepository _categoryRepository;
+        public readonly IMapper _mapper;
+
+        public CategoryService(CategoryRepository categoryRepository, IMapper mapper) {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<Category>> GetAllCategories() {
+            return await _categoryRepository.GetAllCategories();
+        }
+
+        public async Task<Category> GetCategoryById(int id) {
+            return await _categoryRepository.GetCategoryById(id);
+        }
+
+        public async Task<(ServiceResult Result, Category CreatedCategory)> AddCategory(CategoryDTO categoryDTO) {
+            try {
+                var category = _mapper.Map<Category>(categoryDTO);
+                var createdCategory = await _categoryRepository.AddCategory(category);
+                return (ServiceResult.Ok(), createdCategory);
+            }
+            catch (Exception ex) {
+                return (ServiceResult.Fail($"Error: {ex.Message}"), null);
+            }
+        }
+
+        public async Task<ServiceResult> UpdateCategory(Category category) {
+            try {
+                await _categoryRepository.UpdateCategory(category);
+                return ServiceResult.Ok();
+            }
+            catch (Exception ex) {
+                if (ex.Message.Contains(Constants.NOT_FOUND_ERROR)) {
+                    return ServiceResult.Fail($"Category {Constants.NOT_FOUND_ERROR}", Enums.Status.NotFound);
+                }
+                return ServiceResult.Fail($"{Constants.UNEXPECTED_ERROR}{ex.Message}");
+            }
+        }
+
+        public async Task<ServiceResult> DeleteCategory(Category category) {
+            try {
+                await _categoryRepository.DeleteCategory(category);
+                return ServiceResult.Ok();
+            }
+            catch (Exception ex) {
+                return ServiceResult.Fail($"{Constants.UNEXPECTED_ERROR}{ex.Message}");
+            }
+        }
+    }
+}
--- LostAndFoundItems.BLL/obj/Debug/net8.0/LostAndFoundItems.BLL.AssemblyInfo.cs
+++ LostAndFoundItems.BLL/obj/Debug/net8.0/LostAndFoundItems.BLL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LostAndFoundItems.BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+536e710bbf7ee5a72014faa12584a2ff1cc66a0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+708d749c53d339a34180ae803b936f11ff09ad12")]
 [assembly: System.Reflection.AssemblyProductAttribute("LostAndFoundItems.BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LostAndFoundItems.BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
