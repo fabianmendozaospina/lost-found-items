ab3c2e5ae2f43d4a4841b711073e75c7b179c3c2
Lateef Adeyekun <lateef.adeyekun@gmail.com>
2025-04-10 10:28:38 -0500

    Location Repo and service added

---
+++ LostAndFoundItems.BLL/LocationService.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using LostAndFoundItems.Common;
+using LostAndFoundItems.Common.DTOs;
+using LostAndFoundItems.DAL;
+using LostAndFoundItems.Models;
+
+namespace LostAndFoundItems.BLL {
+    public  class LocationService {
+        public readonly LocationRepository _locationRepository;
+        public readonly IMapper _mapper;
+
+        public LocationService(LocationRepository locationRepository, IMapper mapper) {
+            _locationRepository = locationRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<Location>> GetAllLocations() {
+            return await _locationRepository.GetAllLocations();
+        }
+
+        public async Task<Location> GetLocationById(int id) {
+            return await _locationRepository.GetLocationById(id);
+        }
+
+        public async Task<(ServiceResult Result, Location CreatedLocation)> AddLocation(LocationDTO locationDTO) {
+            try {
+                var location = _mapper.Map<Location>(locationDTO);
+                var createdLocation = await _locationRepository.AddLocation(location);
+                return (ServiceResult.Ok(), createdLocation);
+            }
+            catch (Exception ex) {
+                return (ServiceResult.Fail($"Error: {ex.Message}"), null);
+            }
+        }
+
+        public async Task<ServiceResult> UpdateLocation(Location location) {
+            try {
+                await _locationRepository.UpdateLocation(location);
+                return ServiceResult.Ok();
+            }
+            catch (Exception ex) {
+                if (ex.Message.Contains(Constants.NOT_FOUND_ERROR)) {
+                    return ServiceResult.Fail($"Location {Constants.NOT_FOUND_ERROR}", Enums.Status.NotFound);
+                }
+                return ServiceResult.Fail($"{Constants.UNEXPECTED_ERROR}{ex.Message}");
+            }
+        }
+
+        public async Task<ServiceResult> DeleteLocation(Location location) {
+            try {
+                await _locationRepository.DeleteLocation(location);
+                return ServiceResult.Ok();
+            }
+            catch (Exception ex) {
+                return ServiceResult.Fail($"{Constants.UNEXPECTED_ERROR}{ex.Message}");
+            }
+        }
+    }
+}
---
+++ LostAndFoundItems.DAL/LocationRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using LostAndFoundItems.Common;
+using LostAndFoundItems.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LostAndFoundItems.DAL {
+    public class LocationRepository {
+        private readonly LostAndFoundDbContext _context;
+
+        public LocationRepository(LostAndFoundDbContext context) {
+            _context = context;
+        }
+
+        public async Task<List<Location>> GetAllLocations() {
+            return await _context.Locations
+                .Include(l => l.FoundItems)
+                .Include(l => l.LostItems)
+                .ToListAsync();
+        }
+
+        public async Task<Location> GetLocationById(int id) {
+            return await _context.Locations
+                .Include(l => l.FoundItems)
+                .Include(l => l.LostItems)
+                .SingleOrDefaultAsync(l => l.LocationId == id);
+        }
+
+        public async Task<Location> AddLocation(Location location) {
+            _context.Locations.Add(location);
+            await _context.SaveChangesAsync();
+            return location;
+        }
+
+        public async Task UpdateLocation(Location location) {
+            Location existingLocation = await _context.Locations.FirstOrDefaultAsync(l => l.LocationId == location.LocationId);
+            if (existingLocation == null) {
+                throw new Exception(Constants.NOT_FOUND_ERROR);
+            }
+            existingLocation.Name = location.Name;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteLocation(Location location) {
+            if (location != null) {
+                _context.Locations.Remove(location);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
